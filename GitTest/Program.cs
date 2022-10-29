0c60f887dfec4827ec448c2059fa375ab3668ed7
Pol1nka <akulam75@gmail.com>
2022-10-29 15:25:44 +0500

    first commit

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+    namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world!";
+            foreach(char c in str)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
